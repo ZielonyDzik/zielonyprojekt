0eec494dbad00ab9b52332994cb6e6bee2d907bb
ZielonyDzik <90155847+ZielonyDzik@users.noreply.github.com>
2023-10-11 12:52:07 +0200

    Optymalizacja administracja.php
    - Dodanie danych do bazy danych
    - Wyświetlanie danych bezpośrednio z bazy danych
    - Skrócenie kodu HTML

--- podstrony/administracja.php
+++ podstrony/administracja.php
@@ -1,28 +1,37 @@
 <div class="swiper-slide">
         <table>
-                    <td>
-                        <img src="img/fiveways2.jpg" class="photo">
-                    </td>
-                    <td>
-                <section class="text">
+            <td>
+                <img src="img/fiveways2.jpg" class="photo">
+            </td>
+            <td>
+        <section class="text">
+        <p class="text_title" style="margin-top:0; margin-bottom: 5px;">💠 FiveWays - Administracja 💠</p>
+    <table class="admin_table">
+    
+    <tr>
+    <?php
+$a = new mysqli("localhost","root","","zielonyprojekt");
+$a->query("SET NAMES 'utf8'");
+$d = 0;
+if ($b=$a->query("SELECT id, nick, ranga, discord_id, data_dolaczenia, avatar, klasa FROM administracja;"))
+{
+    while($c=$b->fetch_assoc())
+    {
+        // echo "".$c["nick"]."test";
+        echo "<td><div class='kafelek'><img src='/img/avatary/".$c["avatar"]."' class='kafelek_photo'><br><span class='".$c["klasa"]."'>".$c["nick"]."<br>".$c["ranga"]."</span></div></td>";
+        $d = $d + 1;
+        if(($d % 3) == 0 && ($d < 9)){
+            echo("</tr><tr>");
+        }
+    }
+    $b->close();
+}
+$a->close();
 
-                <p class="text_title" style="margin-top:0; margin-bottom: 5px;">💠 FiveWays - Administracja 💠</p>
-            <table class="admin_table">
-                <tr>
-                <td><div class="kafelek"><img src="/img/avatary/zielonydzik.png" class="kafelek_photo"><br><span class="owner">Zielony Dzik<br>Właściciel</span></div></td>
-                <td><div class="kafelek"><img src="/img/avatary/ivan.png" class="kafelek_photo"><br><span class="head_admin">IVAN<br>Head-Admin</span></div></td>
-                <td><div class="kafelek"><img src="/img/avatary/buli.png" class="kafelek_photo"><br><span class="moderator">Buli<br>Moderator</span></div></td>
-</tr><tr>
-                <td><div class="kafelek"><img src="/img/avatary/kreciak.png" class="kafelek_photo"><br><span class="moderator">Kreciak<br>Moderator</span></div></td>
-                <td><div class="kafelek"><img src="/img/avatary/hyper.png" class="kafelek_photo"><br><span class="junior_mod">HyPer<br>Junior-Mod</span></div></td>
-                <td><div class="kafelek"><img src="/img/avatary/outsider.gif" class="kafelek_photo"><br><span class="junior_mod">OutSider<br>Junior-Mod</span></div></td>
-</tr><tr>
-                <td><div class="kafelek"><img src="/img/avatary/dyziopianka.gif" class="kafelek_photo"><br><span class="junior_mod">Dyziopianka<br>Junior-Mod</span></div></td>
-                <td><div class="kafelek"><img src="/img/avatary/pingwinio.png" class="kafelek_photo"><br><span class="junior_mod">Pingwinio<br>Junior-Mod</span></div></td>
-                <td><div class="kafelek"><img src="/img/avatary/wikfil.png" class="kafelek_photo"><br><span class="junior_mod">Wikfil<br>Junior-Mod</span></div></td>
-</tr>
-</table>
-                </section>
-                    </td>
-            </table>
-        </div>
+    ?>
+    </tr>
+    </table>
+        </section>
+            </td>
+    </table>
+</div>
