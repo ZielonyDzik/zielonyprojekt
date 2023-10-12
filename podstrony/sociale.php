9a486cc52407a63d311de64fa70f6ccb1ce7ae63
ZielonyDzik <90155847+ZielonyDzik@users.noreply.github.com>
2023-10-12 11:30:48 +0200

    Update sociale.php

--- podstrony/sociale.php
+++ podstrony/sociale.php
@@ -7,7 +7,7 @@
                     <td>
                 <section class="text">
                     <p class="text_title" style="font-size: 28pt; margin-top: 10px;">🎥 Kanał YouTube 🎥</p>
-                    <ul style="text-align:left;">
+                    <ul class="text_left">
                     <p class="text_title" style="margin-bottom: 20px;">📜 Informacje:
                     <li>Data dołączenia: 18.07.2015</li>
                     <li>Lokalizacja: Polska</li><br>
@@ -15,15 +15,18 @@
                     <li>Link do kanału - <a href="https://www.youtube.com/@ZielonyDzik">https://www.youtube.com/@ZielonyDzik</a></li><br>
 
                     <li>Zielony Dzik to mój główny kanał, na którym pojawiają się filmy i transmisje na żywo - głównie z gier [ale nie tylko] :D</li>
-</p></ul>
+
+                    </p>                </ul>
+
                 <p class="p_text">
-                <ul style="text-align:left;">
+                <ul class="text_left">
                 <p class="text_title" style="margin-bottom: 20px;">📸 Social Media:</p>
-                <li>Discord - "Zielony Las" - <a href="https://discord.gg/Nm6Kqzb">https://discord.gg/Nm6Kqzb</a></li><br>
-                <li>TikTok - "zielonydzikyt" - <a href="https://www.tiktok.com/@zielonydzikyt">https://www.tiktok.com/@zielonydzikyt</a></li><br>
-                <li>Twitch - "zielonydziks" - <a href="https://www.twitch.tv/zielonydziks">https://www.twitch.tv/zielonydziks</a></li><br>
-                <li>Instagram - "zielonydzik" - <a href="https://www.instagram.com/zielonydzik/">https://www.instagram.com/zielonydzik/</a></li><br>
-</p></ul>
+                <li>Discord - "Zielony Las" - <a href="https://discord.gg/Nm6Kqzb">https://discord.gg/Nm6Kqzb</a></li>
+                <li>TikTok - "zielonydzikyt" - <a href="https://www.tiktok.com/@zielonydzikyt">https://www.tiktok.com/@zielonydzikyt</a></li>
+                <li>Twitch - "zielonydziks" - <a href="https://www.twitch.tv/zielonydziks">https://www.twitch.tv/zielonydziks</a></li>
+                <li>Instagram - "zielonydzik" - <a href="https://www.instagram.com/zielonydzik/">https://www.instagram.com/zielonydzik/</a></li>
+</ul>
+            </p>
                 </section>
                     </td>
             </table>
